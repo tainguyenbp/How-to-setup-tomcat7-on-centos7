262b1992a9a8b62c04c68c806a8bbecc4a55b8b0
root <root@localhost.localdomain>
2018-09-14 23:01:55 +0700

    Done tool setup service tomcat http on centos7

---
+++ tomcat-http/bin/setenv.sh
@@ -0,0 +1,1 @@
+JAVA_OPTS="-Djava.awt.headless=true -Dfile.encoding=UTF-8 -Xms512m -Xmx512m -XX:NewSize=256m -XX:MaxNewSize=256m -XX:PermSize=256m -XX:MaxPermSize=256m -XX:+DisableExplicitGC -XX:+UseConcMarkSweepGC"
